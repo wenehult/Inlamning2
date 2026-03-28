682e676dca84427225b92d00cc6049c43364499c
wenehult <lucas.wenehult@student.ksgyf.se>
2026-03-28 13:27:00 +0100

    Add files via upload

---
+++ bokhandel_backup.sql
@@ -0,0 +1,211 @@
+-- MySQL dump 10.13  Distrib 9.5.0, for Win64 (x86_64)
+--
+-- Host: localhost    Database: Bokhandel
+-- ------------------------------------------------------
+-- Server version	9.5.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+--
+-- Table structure for table `bestallningar`
+--
+
+DROP TABLE IF EXISTS `bestallningar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bestallningar` (
+  `OrderID` int NOT NULL AUTO_INCREMENT,
+  `KundID` int NOT NULL,
+  `Datum` date NOT NULL,
+  `Totalbelopp` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`OrderID`),
+  KEY `KundID` (`KundID`),
+  CONSTRAINT `bestallningar_ibfk_1` FOREIGN KEY (`KundID`) REFERENCES `kunder` (`KundID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bestallningar`
+--
+
+LOCK TABLES `bestallningar` WRITE;
+/*!40000 ALTER TABLE `bestallningar` DISABLE KEYS */;
+INSERT INTO `bestallningar` VALUES (1,1,'2026-03-01',299.99),(2,1,'2026-03-02',599.98),(3,2,'2026-03-03',299.99),(4,3,'2026-03-04',299.99),(5,1,'2026-03-05',299.99);
+/*!40000 ALTER TABLE `bestallningar` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bocker`
+--
+
+DROP TABLE IF EXISTS `bocker`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bocker` (
+  `ISBN` bigint NOT NULL,
+  `Titel` varchar(100) NOT NULL,
+  `Forfattare` varchar(100) NOT NULL,
+  `Pris` decimal(10,2) NOT NULL,
+  `Lagerstatus` int NOT NULL,
+  PRIMARY KEY (`ISBN`),
+  CONSTRAINT `bocker_chk_1` CHECK ((`Pris` > 0))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bocker`
+--
+
+LOCK TABLES `bocker` WRITE;
+/*!40000 ALTER TABLE `bocker` DISABLE KEYS */;
+INSERT INTO `bocker` VALUES (9780261102385,'Sagan om ringen','J.R.R. Tolkien',299.99,20),(9780261103344,'Hobbit','J.R.R. Tolkien',299.99,10),(9780261103597,'Konungens återkomst','J.R.R. Tolkien',299.99,15);
+/*!40000 ALTER TABLE `bocker` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `kunder`
+--
+
+DROP TABLE IF EXISTS `kunder`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `kunder` (
+  `KundID` int NOT NULL AUTO_INCREMENT,
+  `Namn` varchar(100) NOT NULL,
+  `Epost` varchar(255) NOT NULL,
+  `Telefon` varchar(30) NOT NULL,
+  `Adress` varchar(100) NOT NULL,
+  PRIMARY KEY (`KundID`),
+  UNIQUE KEY `Epost` (`Epost`),
+  KEY `idx_epost` (`Epost`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `kunder`
+--
+
+LOCK TABLES `kunder` WRITE;
+/*!40000 ALTER TABLE `kunder` DISABLE KEYS */;
+INSERT INTO `kunder` VALUES (1,'Adam Stensson','adam@epost.se','070000000','Storavägen 23'),(2,'Stefan Berg','stefan@epost.se','070000001','Storavägen 24'),(3,'Robert','robert@epost.se','070000002','gatan 4'),(4,'Lena','lena@epost.se','070000003','gatan 5');
+/*!40000 ALTER TABLE `kunder` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `trg_logga_ny_kund` AFTER INSERT ON `kunder` FOR EACH ROW BEGIN
+    INSERT INTO KundLogg (KundID, Namn, Epost)
+    VALUES (NEW.KundID, NEW.Namn, NEW.Epost);
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `kundlogg`
+--
+
+DROP TABLE IF EXISTS `kundlogg`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `kundlogg` (
+  `LoggID` int NOT NULL AUTO_INCREMENT,
+  `KundID` int DEFAULT NULL,
+  `Namn` varchar(100) DEFAULT NULL,
+  `Epost` varchar(255) DEFAULT NULL,
+  `Registrerad` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`LoggID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `kundlogg`
+--
+
+LOCK TABLES `kundlogg` WRITE;
+/*!40000 ALTER TABLE `kundlogg` DISABLE KEYS */;
+/*!40000 ALTER TABLE `kundlogg` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orderrader`
+--
+
+DROP TABLE IF EXISTS `orderrader`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orderrader` (
+  `OrderradID` int NOT NULL AUTO_INCREMENT,
+  `OrderID` int NOT NULL,
+  `ISBN` bigint NOT NULL,
+  `Antal` int NOT NULL,
+  PRIMARY KEY (`OrderradID`),
+  KEY `OrderID` (`OrderID`),
+  KEY `ISBN` (`ISBN`),
+  CONSTRAINT `orderrader_ibfk_1` FOREIGN KEY (`OrderID`) REFERENCES `bestallningar` (`OrderID`),
+  CONSTRAINT `orderrader_ibfk_2` FOREIGN KEY (`ISBN`) REFERENCES `bocker` (`ISBN`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orderrader`
+--
+
+LOCK TABLES `orderrader` WRITE;
+/*!40000 ALTER TABLE `orderrader` DISABLE KEYS */;
+INSERT INTO `orderrader` VALUES (1,1,9780261103344,1),(2,2,9780261102385,2),(3,3,9780261103597,1),(4,4,9780261103344,1),(5,5,9780261102385,1);
+/*!40000 ALTER TABLE `orderrader` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `trg_minska_lager` AFTER INSERT ON `orderrader` FOR EACH ROW BEGIN
+    UPDATE Bocker
+    SET Lagerstatus = Lagerstatus - NEW.Antal
+    WHERE ISBN = NEW.ISBN;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-03-28 13:24:45
